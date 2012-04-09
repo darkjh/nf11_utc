38db505828c7a7240dae7b93aa619daf2fe9de36
Han JU <ju.han.felix@gmail.com>
2012-04-10 00:23:33 +0200

    test tp 2011 b

--- src/regexp/TestTp_2011.java
+++ src/regexp/TestTp_2011.java
@@ -32,7 +32,35 @@ public class TestTp_2011 {
 			//System.out.println("Après : " + text.substring(m.end()));
 		}
 	}
+	
+public static void Q_B() {
+		
+		// Question 1
+//		String patternString = "(\\b(\\d{3})\\d(?!\\2)\\d{3}\\b)";
+//		String text = "Non : 1256125 Ok : 1233124 non : 121451 ok : 7894785 ok : 9874789";
+
+		// Question 2
+		String patternString = "(\\b(?=\\d*?2\\d*?\\b)(?=\\d*?1\\d*?1\\d*?\\b)\\d{10}\\b)"; //
+		String text = "Non : 135792814 Ok1 : 0234167891 Non : 0123456790 Ok2 : 0315128201";
+		
+		Pattern p = Pattern.compile(patternString);
+		Matcher m = p.matcher(text);
+		System.out.println("R.E.: " + patternString);
+		System.out.println("Test: " + text);
+		//boolean found = m.find();
+		boolean f;
+		while (f = m.find()) {
+			System.out.println("==========================================");
+			System.out.println("Succès : " + f);
+			//System.out.println("Position début : " + m.start());
+			//System.out.println("Avant : " + text.substring(0,m.start()));
+			System.out.println("Sélection : " + m.group());
+			//System.out.println("Groupe : " + m.group(1));
+			//System.out.println("Position fin : " + m.end());
+			//System.out.println("Après : " + text.substring(m.end()));
+		}
+	}
 	public static void main(String[] args) {
-		Q_A();
+		Q_B();
 	}
 }

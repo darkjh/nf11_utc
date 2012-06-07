3477b2f267fcba954cde3cf4d238cbc66d6a5d40
Hao <julien19890118@gmail.com>
2012-06-07 23:23:41 +0200

    add procedure files

---
+++ src/logoparsing/LogoProcedureParameter.java
@@ -0,0 +1,25 @@
+package logoparsing;
+
+public class LogoProcedureParameter {
+	private String nomParam;
+	private double valeurParam;
+	
+	public String getNom() {
+		return nomParam;
+	}
+	public void setNom(String nom) {
+		this.nomParam = nom;
+	}
+	
+	public double getValeur() {
+		return valeurParam;
+	}
+	public void setValeur(double valeur) {
+		this.valeurParam = valeur;
+	}
+	
+	public LogoProcedureParameter(String nomParam, double valeur){
+		this.nomParam = nomParam;
+		this.valeurParam = valeur;
+	}
+}
